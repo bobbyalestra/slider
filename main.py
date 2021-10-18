5bbec3dbbbf5f75dd2a4d9da141f3b719a93c16d
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-18 09:17:38 -0400

    vertical and horizontal sliders

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ root.title("Coding With Doc")
 vertical = Scale(root, from_=0, to=200)
 vertical.pack()
 
+horizontal = Scale(root, from_=0, to=200, orient=HORIZONTAL)
+horizontal.pack()
+
 
 root.mainloop()
