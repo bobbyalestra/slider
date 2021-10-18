210601933c2cc3bf123f0e92a87211de591a2ec1
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-18 09:26:13 -0400

    vertical and horizontal sliders made window bigger

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ vertical.pack()
 horizontal = Scale(root, from_=0, to=200, orient=HORIZONTAL)
 horizontal.pack()
 
+my_label = Label(root, text=horizontal.get()).pack()
+
+def slide():
+    my_label=Label(root, text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get()) + "x400")
+
+my_button = Button(root, text="click me", command=slide).pack()
 
 root.mainloop()
